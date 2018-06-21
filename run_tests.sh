d402049829687751b2ab9bd5e9b66154352bc69e
langmm <cfh5058@gmail.com>
2018-06-21 18:32:02 +0000

    Added models and tests for all fakeplant models

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Run original model
+python growth.py Input/photosynthesis_rate.txt Output/growth_rate.txt
+
+# Run integrations with files
+cisrun tests/test_growth.yml
+cisrun tests/test_light.yml
+cisrun tests/test_photosynthesis.yml
+cisrun tests/test_canopy.yml
+
+# Run complete integration network
+cisrun tests/test_network.yml
